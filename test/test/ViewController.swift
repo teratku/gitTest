372f8905718867f662a1fd85c800ae7e44a791fa
teratku <little_busters_rin_takuya@yahoo.co.jp>
2021-01-04 08:54:15 +0900

    初期コミット

---
+++ test/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 寺田拓弥 on 2021/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
