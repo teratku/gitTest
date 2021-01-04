c58e1ff89ab1027014839ef17d20a57399fc794e
teratku <little_busters_rin_takuya@yahoo.co.jp>
2021-01-04 12:02:27 +0900

    mainView
    textLabel outlet接続

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var textActionButton: UIButton!
+    @IBOutlet weak var textLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
